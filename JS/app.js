9895ca3b5dc9eab2f38b7ef144f083648da588e6
Suman-um <suman.mogali2005@gmail.com>
2026-08-08 19:02:42 +0530

    JS - Datatypes , Literals , console.log()

---
+++ JS/app.js
@@ -0,0 +1,11 @@
+//to print on console 
+console.log("Hello , I m learnig JS");
+
+//literals
+let pencil = 5 ;
+let pen = 10 ;
+
+// let total = "Total item :" + (pencil + pen);
+//we use `` backtick to simplify stirng outputs  and template literals 
+let total = `Total item are ${pencil + pen}`;
+console.log(total);
