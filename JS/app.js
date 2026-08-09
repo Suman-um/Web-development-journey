c73bc09f63035c08feef59730315ac99b404ae6d
Suman-um <suman.mogali2005@gmail.com>
2026-08-09 19:56:14 +0530

    JS alerts, warnings , error msgs in consoles

--- JS/app.js
+++ JS/app.js
@@ -9,3 +9,42 @@ let pen = 10 ;
 //we use `` backtick to simplify stirng outputs  and template literals 
 let total = `Total item are ${pencil + pen}`;
 console.log(total);
+
+//Airthmetic operators
+let a = 10 ;
+let b = 5 ;
+console.log(a+b) ;
+console.log(a-b) ;
+console.log(a/b) ;
+console.log(a%b) ;
+console.log(a**b) ;
+
+//Create a traffic light system using if else statement
+let color = "red" ; // yellow green
+if(color === "red")
+{
+    console.log("Stop") ;
+}
+else if(color == "yellow")
+    console.log("Get ready") ;
+else
+    console.log("Go") ;
+
+//Qn2 A good string is a string which starts with letter a and has length greater than 3
+let str = "apple" ;
+if(str[0] === 'a' && str.length > 3)
+{
+    console.log("Good String") ;
+}
+
+//Alert - this displays an alert msg on the browser
+alert("Somethig is wrong") ;
+
+//prompt - this takes input from the user using a dialog box
+let name = prompt("Enter your name:") ;
+console.log(name) ;
+
+//to print an error msg in the console
+console.error("Danger!Something is wrong") ;
+//to print a warning msg in the console
+console.warn("Warning!") ;
