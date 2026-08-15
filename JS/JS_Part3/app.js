6df35c9fb7a1664754ee4c8aeaa8d0d667763904
Suman-um <suman.mogali2005@gmail.com>
2026-08-15 21:11:46 +0530

    JS - Objects , Array of Objects , Math Objects

--- JS/JS_Part3/app.js
+++ JS/JS_Part3/app.js
@@ -48,7 +48,60 @@ obj[1] //a
 obj[2] //b 
 console.log(obj[1]) ;
 
-//To add or update a value
+//To update a value
 twitterPost.username = "@suraj" ;
-//to add new value
+//to add new value - same procedure 
 twitterPost.gender = "female" ;
+//to delete 
+delete twitterPost.gender
+
+
+//Objects of Objects -to store information of mutliple students
+
+let stus = 
+{
+    aman :
+    {
+        grade : 4 ,
+        cgpa : 7.8 
+    } ,
+    geeta :{
+        grade : 6,
+        cgpa : 8
+    } ,
+    sandu :
+    {
+        grade : 9 ,
+        cgpa : 7 
+    } ,
+} ;
+
+//Array of Objects
+let stus = 
+[
+   
+    {
+        name : "aman" ,
+        grade : 4 ,
+        cgpa : 7.8 
+    } ,
+     
+    {
+        name : "geeta" ,
+        grade : 6,
+        cgpa : 8
+    } ,
+     
+    {
+        name : "sandu" ,
+        grade : 9 ,
+        cgpa : 7 
+    } ,
+ ] ;
+
+ //Math - Object in JS which is a collection of properties - Math.PI and Math.E and
+ //  methods like max , ceil , sqrt , floor , random (gives values from 0 to 1) ,abs
+ 
+//Qn - Generate a random number from 1 to 10 
+Math.floor(Math.random() * 10) + 1 ;
+//We did +1 to make it a range from 1 to 10 
