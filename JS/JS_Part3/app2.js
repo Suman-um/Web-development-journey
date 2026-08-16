2d0285bb878cc09653565171db557eb93ac0d1a9
Suman-um <suman.mogali2005@gmail.com>
2026-08-17 00:33:31 +0530

    Guess Game - Key improvement

--- JS/JS_Part3/app2.js
+++ JS/JS_Part3/app2.js
@@ -1,23 +1,47 @@
-//Users enter a max number and then tries to guess the numbr from 1 to max.
+// User enters a max number and then tries to
+// guess the number from 1 to max.
 
-const max = prompt("Enter your Max number:") ;
+const max = Number(prompt("Enter your Max number:"));
 
-const random = Math.floor(Math.random() * max) + 1 ;
-let guess = prompt("Guess the number:") ;
+const random = Math.floor(Math.random() * max) + 1;
+
+let guess = prompt("Guess the number:");
 
 while(true)
 {
+    
     if(guess == "quit")
     {
-        break ;
+        console.log("You quit the game!");
+        break;
     }
 
-    if(guess == random)
+    // Convert guess from String to Number
+    guess = Number(guess);
+
+    // Correct guess
+    if(guess === random)
     {
-        console.log("Congrats!You guessed it right!The number was", random) ;
+        console.log(
+            "Congrats! You guessed it right! The number was",
+            random
+        );
+        break;
     }
+
+    // Guess is smaller
+    else if(guess < random)
+    {
+        guess = prompt(
+            "Number is too small. Try a larger number."
+        );
+    }
+
+    // Guess is larger
     else
     {
-        console.log("Guesses Wrong!PLease try again!") ;
+        guess = prompt(
+            "Number is too large. Try a smaller number."
+        );
     }
 }
