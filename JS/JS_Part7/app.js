a49d364d994f130ac0b1a5fcff49ac1614a4db36
Suman-um <suman.mogali2005@gmail.com>
2026-09-04 00:39:29 +0530

    JS - DOM

--- JS/JS_Part7/app.js
+++ JS/JS_Part7/app.js
@@ -1,5 +1,11 @@
-console.log(window) ;
-//it is an inbuilt object in JS which is the global object in browser environment
-console.log(document) ;
-//it is an inbuilt object in JS
-console.dir(document);
+//innertext //innerhtml //innerContent
+//getElementById 
+//get ElementByClassName 
+//get ElementByTagName
+//document.querySelector
+
+//data manipulation
+let para = document.querySelector('p') ;
+para.innerText = "abc" ;
+para.innerText = "Hi! Im Peter Parker";
+para.innerHTML = "<b>Hi! Im Peter Parker</b>";
---
+++ JS/JS_Part7/appi.js
@@ -0,0 +1,7 @@
+console.log(window) ;
+//it is an inbuilt object in JS which is the global object in browser environment
+console.log(document) ;
+//it is an inbuilt object in JS
+console.dir(document);
+
+
