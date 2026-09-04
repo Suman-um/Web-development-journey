4fa9fc3d6fc53329a21842471d1678eb2ac6a0a7
Suman-um <suman.mogali2005@gmail.com>
2026-09-04 19:41:34 +0530

    DOM Manipulation

--- JS/JS_Part7/app.js
+++ JS/JS_Part7/app.js
@@ -9,3 +9,37 @@ let para = document.querySelector('p') ;
 para.innerText = "abc" ;
 para.innerText = "Hi! Im Peter Parker";
 para.innerHTML = "<b>Hi! Im Peter Parker</b>";
+
+
+//obj.getAttribute('attributeName') //getAttribute
+//obj.style 
+heading = document.querySelector('h1') ;
+heading.style.color = "green" ; //inline style 
+
+//obj.classList.add('className') //add class
+heading.classList.add('highlight') ;
+
+//heading.classList.remove('highlight') ; //remove class
+//heading.classList.toggle('highlight') ;
+//heading.classList.contains('highlight') ; // true 
+
+//Navigation 
+//obj.parentElement //parent element
+//obj.children //child elements
+//obj.nextElementSibling //next sibling
+//obj.previousElementSibling //previous sibling
+//obj.childElementCount //child element count
+
+
+// add elements
+let newP = document.createElement('p') ;
+console.dir(newP) ;
+newP.innerText = "This is a new paragraph" ;
+
+//to insert
+let bo = document.querySelector('body') ;
+bo.appendChild(newP) ;
+
+let button = document.createElement('button') ;
+button.innerText = "Click me" ;
+button.appendChild(para) ;
