f38966282e9decb7415532983f324bc783e44deb
Suman-um <suman.mogali2005@gmail.com>
2026-09-07 01:27:11 +0530

    JS - Event Listeners

--- JS/JS_Part8/app.js
+++ JS/JS_Part8/app.js
@@ -26,9 +26,38 @@ let btns = document.querySelectorAll("button") ;
 // }
 
 //for multiple values or functions to be executed, we can use addEventListener
-for(let b of btns)
+
+// for(let b of btns)
+// {
+//     b.addEventListener("click" , sayHello) ;
+//     b.addEventListener("click" , sayHello2) ;
+// }
+
+//Activity - Generate a random colour whenever you click on the button
+
+let gbtn = document.querySelector("button") ;
+
+gbtn.addEventListener("click" , function()
 {
-    b.addEventListener("click" , sayHello) ;
-    b.addEventListener("click" , sayHello2) ;
+  console.log("Button was clicked!") ;
+  let h3 = document.querySelector("h3") ;
+  let randomColor = getRandomColor() ;
+  h3.innerText = randomColor ;
+  let div = document.querySelector("div") ;
+  div.style.backgroundColor = `rgb${randomColor}` ;
+}) ;
+
+function getRandomColor()
+{
+    let red = Math.floor(Math.random() * 255) ;
+    let green = Math.floor(Math.random() * 255) ;
+    let blue = Math.floor(Math.random() * 255) ;
+
+    let color = `(${red} , ${green} , ${blue})` ;
+    return color ;
 }
+
+
+
+
 
